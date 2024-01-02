454207ea5b8bc88b3dd8441c8db3c859f5223340
Fabio Castilhos <fabcastilhos@gmail.com>
2024-01-02 17:40:25 -0300

    Add files via upload

---
+++ Webhooks/HelpDesk/config.php
@@ -0,0 +1,18 @@
+<?php
+	
+	/**	  
+	 * @author Fabio Castilhos <fabcastilhos@gmail.com> 
+	 * @copyright GNU © 2020-2023, HappyHouse. 
+	 * @access protected (public, protected e private) 
+	 * @name webhook_helpdesk.php 
+	 * @package Workplace
+	 * @subpackage Workplace/Webhook/HelpDesk/
+	 * @version 1.0	 
+	*/
+	
+	// Variáveis Integração com o Workplace From Meta 	
+	$app_secret   = 'app_secret';
+	$access_token = 'replace_with_your_access_token';
+	$verify_token = 'replace_with_your_verify_token';
+
+?>
